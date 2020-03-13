ff8964b81e1c070c16bad2ba5dba55a5ccd7b243
burnsed <rburns@stic.net>
2020-03-13 13:25:34 -0700

    move to redis

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
+import json
 from flask import Flask, jsonify, request
-import threading
 import time
 
 # Create a new key to use:  POST http://localhost:5000/new/<key>
@@ -9,53 +9,32 @@ import time
 # Get the value of the key: GET http://localhost:5000/<key>
 # Clear key:                GET http://localhost:5000/clear/<key>
 
+
 app = Flask(__name__)
-lock = threading.Lock()
-
-d = {}
-d['data'] = {}
-
-def update_thread():
-    global d
-    while True:
-        with lock:
-            d['uptime'] = d.get('uptime', 0) + 1
-        time.sleep(1.0)
+rdb = redis.from_url(os.environ.get("REDIS_URL"))
 
 @app.route('/clear/<name>', methods=['GET'])
 def clear(name):
-    global d
-    with lock:
-        if name in d['data']:
-            del(d['data'][name])
-        return jsonify({})
+    rdb.set(name, "")
+    return jsonify({name: ""})
 
 # Create a new key
 @app.route('/new/<name>', methods=['POST'])
 def new_key(name):
-    with lock:
-        d['data'][name] = {
-                            'value': "",
-                            'time': time.time()
-                          }
-        return jsonify(d['data'][name])
+    rdb.set(name, "")
+    return jsonify({name: ""})
 
+# set a value
 @app.route('/<name>/<value>', methods=['POST'])
 def set(name, value):
-    global d
-    with lock:
-        d['data'][name] = d['data'].get(name, {})
-        # d['data'][name]['value'] = request.args.get('value') or float('nan')
-        d['data'][name]['value'] = value
-        d['data'][name]['time'] = time.time()
-        return jsonify(d['data'][name])
+    rdb.set(name, value)
+    return jsonify({name:value})
 
+# fetch a value
 @app.route('/<name>', methods=['GET'])
 def get_name(name):
-    with lock:
-        return d['data'].get(name, {}).get('value', "")
+    return rdb.get(name) or ""
 
 
 if __name__ == '__main__':
-    threading.Thread(target=update_thread).start()
-    app.run(threaded=True, debug=True)
+    app.run()
